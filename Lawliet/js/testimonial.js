dcc712a937c3bca9b87d49e30106a59d4d15eee8
Muhsina <muhsinanpm@gmail.com>
2023-03-19 18:44:14 +0530

    uploading

---
+++ Lawliet/js/testimonial.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        rewindNav : false,
+        autoPlay:true
+    });
+});
